84ae1da274b43152f56ab92cbbd63378bf7f5caa
Ivan <iab050811@gmail.com>
2026-03-23 16:27:52 +0100

    Sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+  site: "https://ivanamon.dev",
   integrations: [mdx(), sitemap()],
-
   vite: {
     plugins: [tailwindcss()],
   },
