05537e8a496cbc4f733c92f734d5277b01f3bb8e
Ivan <iab050811@gmail.com>
2026-04-01 14:54:17 +0200

    fix: spelling mistakes

--- src/data/studies.ts
+++ src/data/studies.ts
@@ -3,7 +3,7 @@ export const studies = [
     title: "Telecom Engineering",
     institution: "Universidad Politécnica de Madrid (UPM)",
     description:
-      "Undergraduate degree covering systems programming, backend engineering and computer netoworks. Building a strong foundation in both hardware and software systems.",
+      "Undergraduate degree covering systems programming, backend engineering and computer networks. Building a strong foundation in both hardware and software systems.",
     tags: [
       "Backend",
       "Systems",
