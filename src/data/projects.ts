72c364ad5abc96735eac984a7e630e0fecd464ff
Ivan <iab050811@gmail.com>
2026-05-05 17:47:57 +0200

    refactor: XSCP name changed

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -17,7 +17,7 @@ export const projects: {
 	{
 		title: "XSCP",
 		techStack: "Rust · Low Level · Networking",
-		description: "Text-based chat protocol over TCP. The protocol specification, the server, and the client are all designed and implemented from scratch in Rust, with no external protocol libraries.",
+		description: "Text-based communication protocol over TCP. The protocol specification, the server, and the client are all designed and implemented from scratch in Rust, with no external protocol libraries.",
 		ctaText: "View on GitHub",
 		ctaLink: "https://github.com/ivan-amon/xscp",
 		icons: [rustIcon, tcpipIcon, dockerIcon],
